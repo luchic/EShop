614aac9a4750be073249640029eb1c5fa99fe61b
Nikita Luchinin <nluchini@student.42heilbronn.de>
2026-03-28 12:23:57 +0100

    Add to main setup inMemory repository

--- backend/shop/cmd/main.go
+++ backend/shop/cmd/main.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	apiGood "backend/shop/internal/api/goods"
 	"backend/shop/internal/config"
 	"backend/shop/internal/modules/goods"
 	"log"
@@ -14,7 +15,14 @@ func main() {
 	}
 	_ = cfg
 
-	mux := goods.Routers()
+	repo := goods.NewMemoryRepository([]apiGood.Product{
+		{Id: 1, Name: "Phone", Description: "Smartphone"},
+		{Id: 2, Name: "Keyboard", Description: "Mechanical keyboard"},
+		{Id: 3, Name: "Mouse", Description: "Wireless mouse"},
+		{Id: 4, Name: "Monitor", Description: "27 inch monitor"},
+	})
+
+	mux := goods.Routers(repo)
 	err = http.ListenAndServe(":8080", mux)
 	if err != nil {
 		log.Printf("Couldn't run server: %v", err)
