62156ff1f832e237bcc373a64f19949d3566b423
Maximilian Felix Welsch <mawelsch@student.42heilbronn.de>
2026-03-28 09:56:26 +0100

    added a pageflip system to the catalog to be integrated with item fetching

--- frontend/catalog_script.js
+++ frontend/catalog_script.js
@@ -21,24 +21,38 @@ function CloseDescription() {
 
 function SetItemInvalid(id) {
     document.getElementById(id).style.setProperty('--valid') = 0
-    document.getElementById(id).style.setProperty('background-color') = rgba(0, 0, 0, 0)
+    document.getElementById(id).style.setProperty('opacity') = 0
 }
 
 function SetItemValid(id) {
     document.getElementById(id).style.setProperty('--valid') = 1
-    document.getElementById(id).style.setProperty('background-color') = rgb(204,153,102)
+    document.getElementById(id).style.setProperty('opacity') = 1
 }
 
 function SetItems() {
-    
+    if (page_id == 0) {
+        document.getElementById('LeftArrow').style.setProperty('opacity', 0)
+    }
+    else {
+        document.getElementById('LeftArrow').style.setProperty('opacity', 1)
+    }
+    document.getElementById("pagenum_1").innerHTML = pagenum
+    document.getElementById("pagenum_2").innerHTML = pagenum + 1
 }
 
 function TurnLeftPage() {
+    if (page_id == 0) {
+        return
+    }
     pagenum -= 2
     page_id -= 1
+    console.log(pagenum)
+    SetItems()
 }
 
 function TurnRightPage() {
     pagenum += 2
     page_id += 1
+    console.log(pagenum)
+    SetItems()
 }
