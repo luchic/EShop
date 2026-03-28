b18e7272cc85fbe14de2ca7f32337b5f5e6cf3a0
Nikita Luchinin <nluchini@student.42heilbronn.de>
2026-03-28 13:28:53 +0100

    Update cmd

--- backend/shop/cmd/main.go
+++ backend/shop/cmd/main.go
@@ -1,29 +1,33 @@
-package cmd
+package main
 
 import (
 	apiGood "backend/shop/internal/api/goods"
-	"backend/shop/internal/config"
 	"backend/shop/internal/modules/goods"
+	"fmt"
 	"log"
 	"net/http"
 )
 
 func main() {
-	cfg, err := config.Load()
-	if err != nil {
-		log.Printf("load config: %v", err)
-	}
-	_ = cfg
+	// cfg, err := config.Load()
+	// if err != nil {
+	// 	log.Printf("load config: %v", err)
+	// }
+	// _ = cfg
 
 	repo := goods.NewMemoryRepository([]apiGood.Product{
 		{Id: 1, Name: "Phone", Description: "Smartphone"},
 		{Id: 2, Name: "Keyboard", Description: "Mechanical keyboard"},
 		{Id: 3, Name: "Mouse", Description: "Wireless mouse"},
 		{Id: 4, Name: "Monitor", Description: "27 inch monitor"},
+		{Id: 5, Name: "Printer", Description: "27 inch monitor"},
+		{Id: 6, Name: "Laptop", Description: "27 inch monitor"},
+		{Id: 7, Name: "PC", Description: "27 inch monitor"},
 	})
 
 	mux := goods.Routers(repo)
-	err = http.ListenAndServe(":8080", mux)
+	fmt.Printf("Run server...")
+	err := http.ListenAndServe(":8080", mux)
 	if err != nil {
 		log.Printf("Couldn't run server: %v", err)
 	}
