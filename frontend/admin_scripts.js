d09865e51520476f45341ffda1904f6afe333e0c
Maximilian Felix Welsch <mawelsch@student.42heilbronn.de>
2026-03-29 04:05:32 +0200

    added some more stuff for the catalog, waiting on nikita to come back so he can fix the API stuff

--- frontend/admin_scripts.js
+++ frontend/admin_scripts.js
@@ -2,7 +2,49 @@ function Placeholder() {
     alert("Placeholder")
 }
 
-function AddItem(item_name, item_img, item_desc, item_id, item_price) {
-    alert(item)
-    console.log(item)
+async function AddItem() {
+    let name = document.getElementById("Iname").value;
+    if (!name.trim()) {
+        alert('Please enter all fields!');
+        return;
+    }
+
+    let logo_raw = document.getElementById("Ilogo").files;
+    if (!logo_raw || logo_raw.length === 0) {
+        alert('Please enter all fields!');
+        return;
+    }
+
+    if (!logo_raw[0].type.startsWith("image")) {
+        alert("Please attach an image file");
+        return;
+    }
+
+    // Convert File to Base64 and wait for it
+    const logo = await new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.onload = () => resolve(reader.result.split(',')[1]);
+        reader.onerror = () => reject(reader.error);
+        reader.readAsDataURL(logo_raw[0]);
+    });
+
+    let desc = document.getElementById('Idesc').value;
+    let price = document.getElementById('Iprice').value;
+
+    if (!desc.trim() || !price.trim()) {
+        alert('Please enter all fields!');
+        return;
+    }
+
+    const item = { name, logo, desc, price };
+    const jsonstr = JSON.stringify(item);
+    const response = await fetch('http://localhost:8080/itmes/add', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify(item)
+});
+const data = await response.json();
+    console.log(data)
 }
