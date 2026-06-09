6342f6b42d3a74dfcc9e7f265eb98182151601bd
Nikita Luchinin <nluchini@student.42heilbronn.de>
2026-06-09 18:42:49 +0200

    Add config json file and structer

---
+++ backend/internal/config/config.go
@@ -0,0 +1,26 @@
+package config
+
+import (
+	"encoding/json"
+	"os"
+)
+
+type Config struct {
+	ConnectionString string `json:"connection_string"`
+}
+
+func NewConfig(path string) (*Config, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+
+	defer file.Close()
+
+	var cfg Config
+	if err := json.NewDecoder(file).Decode(&cfg); err != nil {
+		return nil, err
+	}
+
+	return &cfg, nil
+}
