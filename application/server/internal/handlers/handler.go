414ab357e251397f6d1df5bee7d9140cca58303a
Nikita Luchinin <nluchini@student.42heilbronn.de>
2026-06-16 12:37:09 +0200

    Implement session creation

--- application/server/internal/handlers/handler.go
+++ application/server/internal/handlers/handler.go
@@ -4,25 +4,31 @@ import (
 	"fmt"
 	"net/http"
 	"shop/internal/repository"
+
+	"github.com/go-redis/redis/v8"
 )
 
 type Handler struct {
+	redis      *redis.Client
 	repository *repository.Repository
 }
 
-func AddRouter(mux *http.ServeMux, repository *repository.Repository) *http.ServeMux {
+func AddRouter(mux *http.ServeMux, repository *repository.Repository, redis *redis.Client) *http.ServeMux {
 	if mux == nil {
 		return mux
 	}
 
-	handler := newHandler(repository)
+	handler := newHandler(repository, redis)
 	mux.HandleFunc("GET /", handler.handleHome)
 	mux.HandleFunc("POST /user/register", handler.handleRegisterUser)
 	return mux
 }
 
-func newHandler(repository *repository.Repository) *Handler {
-	return &Handler{repository: repository}
+func newHandler(repository *repository.Repository, redis *redis.Client) *Handler {
+	return &Handler{
+		redis:      redis,
+		repository: repository,
+	}
 }
 
 func (h *Handler) handleHome(w http.ResponseWriter, r *http.Request) {
--- application/server/internal/handlers/user.go
+++ application/server/internal/handlers/user.go
@@ -1,12 +1,16 @@
 package handlers
 
 import (
+	"context"
 	"encoding/json"
 	"net/http"
 	"shop/internal/api"
+	"time"
 
 	"golang.org/x/crypto/bcrypt"
 )
+
+const SESSION_DURATION = 30 * time.Minute
 
 func (h *Handler) handleRegisterUser(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
@@ -28,6 +32,98 @@ func (h *Handler) handleRegisterUser(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.WriteHeader(http.StatusCreated)
+}
+
+func (h *Handler) handleLoginUser(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var loginUser api.LoginUser
+	if err := json.NewDecoder(r.Body).Decode(&loginUser); err != nil {
+		http.Error(w, "Couldn't decode json body", http.StatusBadRequest)
+		return
+	}
+
+	if !isLoginUserValid(loginUser) {
+		http.Error(w, "Email and password requiered", http.StatusBadRequest)
+	}
+
+	user, err := h.repository.GetUserByEmail(loginUser.Email)
+	if err != nil {
+		http.Error(w, "Invalid password or Login", http.StatusUnauthorized)
+		return
+	}
+
+	if !verifyPassword(loginUser.Password, user.Password) {
+		http.Error(w, "Invalid password or Login", http.StatusUnauthorized)
+		return
+	}
+
+	sessionId, expiresAt, err := h.createSession(&user)
+	if err != nil {
+		http.Error(w, "Failed to create session", http.StatusInternalServerError)
+	}
+
+	response := api.LoginResponse{
+		UserId:    user.Id,
+		SessionId: sessionId,
+		Email:     user.Email,
+		ExpiresAt: expiresAt.Unix(),
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(response)
+}
+
+func (h *Handler) createSession(user *api.User) (string, time.Time, error) {
+	sessionId, err := generateSessionId()
+	if err != nil {
+		return "", time.Time{}, err
+	}
+
+	timeNow := time.Now()
+	expiredAt := timeNow.Add(SESSION_DURATION)
+
+	sessionData := api.SessionData{
+		UserID:    user.Id,
+		Email:     user.Email,
+		CreatedAt: timeNow,
+		ExpiresAt: expiredAt,
+	}
+
+	sessionJSON, err := json.Marshal(sessionData)
+	if err != nil {
+		return "", time.Time{}, err
+	}
+
+	err = h.redis.Set(
+		context.Background(),
+		sessionId,
+		sessionJSON,
+		SESSION_DURATION).Err()
+	if err != nil {
+		return "", time.Time{}, err
+	}
+
+	return sessionId, expiredAt, nil
+}
+
+func generateSessionId() (string, error) {
+	b := make([]byte, 32)
+	_ = b
+	return "", nil
+}
+
+func isLoginUserValid(loginUser api.LoginUser) bool {
+	_ = loginUser
+	return true
+}
+
+func verifyPassword(login_password string, actual_hash []byte) bool {
+	err := bcrypt.CompareHashAndPassword(actual_hash, []byte(login_password))
+	return err == nil
 }
 
 // Implementation isn't so good.
