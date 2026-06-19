bcb469d5f266b103ba9f7c1e686396234d97f02d
Nikita Luchinin <nluchini@student.42heilbronn.de>
2026-06-19 16:39:30 +0200

    Add new mapper for map products to response products model

--- application/server/internal/product/map.go
+++ application/server/internal/product/map.go
@@ -11,3 +11,17 @@ func MapCreatProductRequestToProduct(requet api.CreateProductRequest) api.Produc
 		ImageUrl:    "",
 	}
 }
+
+func MapProductToGetProductsResponse(products []api.Product) []api.GetProductsResponse {
+	productsResponse := make([]api.GetProductsResponse, len(products))
+	for i := 0; i < len(products); i++ {
+		productsResponse[i] = api.GetProductsResponse{
+			Id:          products[i].Id,
+			Name:        products[i].Name,
+			Description: products[i].Description,
+			Price:       products[i].Price,
+			Stock:       products[i].Stock,
+		}
+	}
+	return productsResponse
+}
