d444191b5a2251f151c16ba47201a198cfb6c50c
Nikita Luchinin <nluchini@student.42heilbronn.de>
2026-06-28 18:01:34 +0200

    Add middleware for session validation

---
+++ application/server/internal/services/auth_middleware.go
@@ -0,0 +1,25 @@
+package services
+
+import (
+	"net/http"
+	"shop/internal/auth"
+)
+
+// Maybe I will change it in future
+func AuthIsReqiuered(
+	authService *auth.Service,
+	handler func(http.ResponseWriter, *http.Request,
+)) func(w http.ResponseWriter, r *http.Request) {
+
+	next := http.HandlerFunc(handler)
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		_, err := authService.ValidateSession(r)
+		if err != nil {
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	}
+}
